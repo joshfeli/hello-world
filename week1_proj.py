6775dd233e3f73a5547b7017af09ded78b383757
Joshua Feliciano <joshfeli613@gmail.com>
2022-06-28 22:59:18 +0000

    Got the response to return a dictionary created from JSON, printed some information obtained from the JSON response

--- week1_proj.py
+++ week1_proj.py
@@ -1,3 +1,4 @@
+from pprint import pprint
 import json
 import requests
 
@@ -8,7 +9,8 @@ API_KEY = 'AIzaSyB0S1oSehFW5v6ICXJ2iaiUDpMhjCIPeeQ'
 api = pyyoutube.Api(api_key=API_KEY)
 
 playlist_item_by_id = api.get_playlist_item_by_id(playlist_item_id="UExPVTJYTFl4bXNJS3BhVjhoMEFHRTA1c28wZkF3d2ZUdy41NkI0NEY2RDEwNTU3Q0M2")
-playlist_item_by_playlist = api.get_playlist_items(playlist_id="PLOU2XLYxmsIKpaV8h0AGE05so0fAwwfTw", count=2)
+playlist_item_by_playlist = api.get_playlist_items(playlist_id="PLOU2XLYxmsIKpaV8h0AGE05so0fAwwfTw", count=2, return_json=True)
 
-print(playlist_item_by_id)
-print(playlist_item_by_playlist)
+for item in playlist_item_by_playlist['items']:
+    print(item['snippet']['channelTitle'])
+    pprint(item['snippet']['description'])
